6f960877de0777575977f22ae7ceb87f87b110a9
Leonardo Abreu <abreu.leo@hotmail.com>
2019-01-24 16:53:53 -0200

    Products Carts

--- site.php
+++ site.php
@@ -69,7 +69,57 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart",[
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
+});
+
+$app->get("/cart/:idproducts/add", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i = 0; $i < $qtd; $i++) {
+
+		$cart->addProduct($product);	
+	}	
+	
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproducts/minus", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproducts/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
 });
 
 ?>
